38dfc8e65a5555afd7ffcc8062a8bdd510edcaad
izemir <laruscrococat@gmail.com>
2019-10-26 21:46:36 +0300

    project added to git

---
+++ Assets/PopupScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupScript : MonoBehaviour
+{
+
+
+
+    public GameObject openButton;
+    
+    public GameObject closeButton;
+
+    public Image[] popups;
+
+    public Text[] texts;
+
+    
+    public void ButtonClicked()
+    {
+        
+        closeButton.SetActive(true);
+
+        openButton.SetActive(false);
+
+        image.enabled = true;
+
+        text.text = "a";
+
+        text.enabled = true;
+
+
+    }
+
+
+    public void StartPopup()
+    {
+        //openButton = transform.Find("OpenButton").GetComponent<Button>();
+        //openButton.interactable = false;
+
+        //GameObject closeButton = GameObject.Find("CloseButton");
+        //closeButton.SetActive(true);
+
+
+    }
+
+
+}
